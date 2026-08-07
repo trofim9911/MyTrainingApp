f7b5b190b55044e2d093d1ece5bca666fa437423
trofim9911 <trofim9911@gmail.com>
2026-08-07 13:34:57 +0300

    step2: add VariablesJava and VariablesKotlin

--- app/src/main/java/com/yourname/training/step2/VariablesKotlin.kt
+++ app/src/main/java/com/yourname/training/step2/VariablesKotlin.kt
@@ -1,6 +1,6 @@
 package com.yourname.training.step2
 
-class VariablesKotlin {
+
     fun main() {
         var age = 25;
         val height = 1.75;
@@ -8,6 +8,10 @@ class VariablesKotlin {
         val isStudent = true;
         val name = "Alice";
         val middleName: String? = null;
-        println("age = $age " + "height = $height " + "gender = $gender " + "isStudent = $isStudent " + "name = $name " + "middleName = ${middleName?: "Не указано"}")
+        println("age = $age")
+        println("height = $height");
+        println("gender = $gender");
+        println("isStudent = $isStudent");
+        println("name = $name ");
+        println("middleName = ${middleName?.length ?: 0}");
     }
-}
